3c48f028d09fdebcc1148e28c631cf806af67c3a
NvidiaGitAccount <pawel.tomasik@pictec.eu>
2019-02-23 16:07:48 +0000

    Laucnher without timeit

--- launcher.sh
+++ launcher.sh
@@ -3,11 +3,11 @@
 cd /home/nvidia/DASH
 python3 GUI.py&
 GUIPID=$!
-python3 runtime.py -audio_config $PWD/configs/large_hop_input_config.yaml -post_filter_config $PWD/configs/null_postfilter.yaml -model_config $PWD/configs/4ch-lstm_mvdr_config.yaml -timeit&
+python3 runtime.py -audio_config $PWD/configs/large_hop_input_config.yaml -post_filter_config $PWD/configs/null_postfilter.yaml -model_config $PWD/configs/4ch-lstm_mvdr_config.yaml&
 RUNTIMEPID=$!
 while kill -0 $GUIPID; do
 	if ! kill -0 $RUNTIMEPID; then
-		python3 runtime.py -audio_config $PWD/configs/large_hop_input_config.yaml -post_filter_config $PWD/configs/null_postfilter.yaml -model_config $PWD/configs/4ch-lstm_mvdr_config.yaml -timeit&
+		python3 runtime.py -audio_config $PWD/configs/large_hop_input_config.yaml -post_filter_config $PWD/configs/null_postfilter.yaml -model_config $PWD/configs/4ch-lstm_mvdr_config.yaml&
 		RUNTIMEPID=$!
 	fi
 	sleep 3
